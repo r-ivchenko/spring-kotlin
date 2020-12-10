c2e599f63f069a6bc3b618f28bffbaeef2ca3169
Roman I <ivchenko.roman.alex@gmail.com>
2020-12-10 15:54:30 +0200

    initial commit

---
+++ src/main/kotlin/box/sand/spring5webapp/Spring5webappApplication.kt
@@ -0,0 +1,11 @@
+package box.sand.spring5webapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring5webappApplication
+
+fun main(args: Array<String>) {
+	runApplication<Spring5webappApplication>(*args)
+}
